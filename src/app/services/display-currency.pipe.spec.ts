fdc4a08d2c903aeed0af01b19b9747a9134dcad6
Handy Hasan <handy.hasan@yahoo.com>
2021-05-09 13:12:26 +1000

    test: currency display pipe

--- src/app/services/display-currency.pipe.spec.ts
+++ src/app/services/display-currency.pipe.spec.ts
@@ -1,8 +1,30 @@
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { TestBed } from '@angular/core/testing';
 import { DisplayCurrencyPipe } from './display-currency.pipe';
 
 describe('DisplayCurrencyPipe', () => {
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [CommonModule],
+    providers: [CurrencyPipe]
+  }));
+
   it('create an instance', () => {
-    const pipe = new DisplayCurrencyPipe();
+    const pipe = new DisplayCurrencyPipe(TestBed.inject(CurrencyPipe));
     expect(pipe).toBeTruthy();
   });
+
+  it('correctly converts a number into Currency-formatted number', () => {
+    const pipe = new DisplayCurrencyPipe(TestBed.inject(CurrencyPipe));
+    const expected = "$23.30";
+    const actual = pipe.transform(23.30);
+    expect(actual).toBe(expected);
+  });
+
+  it('returns default value on falsy input', () => {
+    const pipe = new DisplayCurrencyPipe(TestBed.inject(CurrencyPipe));
+    const expected = "unavailable";
+    const actual = pipe.transform(null);
+    expect(actual).toBe(expected);
+  })
 });
